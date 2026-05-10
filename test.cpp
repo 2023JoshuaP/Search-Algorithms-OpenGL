f1d5ddbde616547e04f6399fff35bedad6214cef
2023JoshuaP <jphilco@unsa.edu.pe>
2026-05-09 21:16:59 -0500

    Udpate.

--- test.cpp
+++ test.cpp
@@ -24,7 +24,7 @@ void display() {
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 800);
+    glutInitWindowSize(1100, 800);
     glutCreateWindow("Puntos");
     glutDisplayFunc(display);
     glutMainLoop();
